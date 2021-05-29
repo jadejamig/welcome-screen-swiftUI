bf9277b4c78ceb7c8c5db5d2f42a22ad6189330a
Jade Jamig <ajamig016@gmail.com>
2021-05-30 01:38:24 +0800

    Initial Commit

---
+++ Welcome Screen/Welcome_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Welcome_ScreenApp.swift
+//  Welcome Screen
+//
+//  Created by Anselm Jade Jamig on 5/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Welcome_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
